70776a06b737445c70d9776c9d0e78e8d5eeaf7c
“Aniket_D” <aniketdakhare@gmail.com>
2020-05-30 23:40:00 +0530

    Monthly wages calculated for 100 days or 20 days

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -6,14 +6,14 @@ public class EmployeeWage
 	static final int PARTTIME=2;
 	static final int WAGE_PER_HOUR=20;
 	static final int WORKING_DAYS=20;
-	static int daily_hours=8;
-	static int perDayWage;
-	static int empWageperMonth;
+	static final int MAX_HOURS=100;
 	void salary()
 	{
+		int daily_hours=0, totalEmpHours=0, totalWorkingDays=0;
 		Random r = new Random();
-		for (int i = 0; i < WORKING_DAYS; i++)
+		while (totalEmpHours < MAX_HOURS && totalWorkingDays < WORKING_DAYS )
 		{
+			totalWorkingDays++;
 			int check = r.nextInt(3);
 			switch (check)
 			{
@@ -26,9 +26,9 @@ public class EmployeeWage
 				default:
 					daily_hours=0;
 			}
-			perDayWage=daily_hours*WAGE_PER_HOUR;
-			empWageperMonth+=perDayWage;
+			totalEmpHours+=daily_hours;
 		}
+		int empWageperMonth=totalEmpHours*WAGE_PER_HOUR;
 		System.out.println("Monthly salary of an employee is Rs. "+empWageperMonth);
 	}
 	public static void main(String[] args)
@@ -37,5 +37,4 @@ public class EmployeeWage
 		EmployeeWage emp=new EmployeeWage();
 		emp.salary();
 	}
-
 }
