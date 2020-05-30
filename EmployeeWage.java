5f00ff5c7b7403398f19622acd74cd49796b141c
“Aniket_D” <aniketdakhare@gmail.com>
2020-05-30 23:11:11 +0530

    Monthly wage calculated

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -5,31 +5,37 @@ public class EmployeeWage
 	static final int FULLTIME=1;
 	static final int PARTTIME=2;
 	static final int WAGE_PER_HOUR=20;
+	static final int WORKING_DAYS=20;
 	static int daily_hours=8;
 	static int perDayWage;
-	void attendance()
+	static int empWageperMonth;
+	void salary()
 	{
 		Random r = new Random();
-		int check = r.nextInt(3);
-		switch (check)
+		for (int i = 0; i < WORKING_DAYS; i++)
 		{
-			case FULLTIME:
-				daily_hours=8;
-				break;
-			case PARTTIME:
-				daily_hours=4;
-				break;
-			default:
-				System.out.println("Employee is Absent");
-				daily_hours=0;
+			int check = r.nextInt(3);
+			switch (check)
+			{
+				case FULLTIME:
+					daily_hours=8;
+					break;
+				case PARTTIME:
+					daily_hours=4;
+					break;
+				default:
+					daily_hours=0;
+			}
+			perDayWage=daily_hours*WAGE_PER_HOUR;
+			empWageperMonth+=perDayWage;
 		}
-		perDayWage=daily_hours*WAGE_PER_HOUR;
-		System.out.println("Per day salary of an employee is Rs. "+perDayWage);
+		System.out.println("Monthly salary of an employee is Rs. "+empWageperMonth);
 	}
 	public static void main(String[] args)
 	{
 		System.out.println("Welcome to Employee Wage Computation Program");
 		EmployeeWage emp=new EmployeeWage();
-		emp.attendance();
+		emp.salary();
 	}
+
 }
