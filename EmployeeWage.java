55a549013964ce5054f22948733d03035c2c7ac3
“Aniket_D” <aniketdakhare@gmail.com>
2020-05-30 20:33:23 +0530

    Attendance Checked

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,9 +1,21 @@
+import java.util.Random;
+
 public class EmployeeWage
 {
 
 	public static void main(String[] args)
 	{
 		System.out.println("Welcome to Employee Wage Computation Program");
+		Random r = new Random();
+		int check = r.nextInt(2);
+		if (check == 1)
+		{
+			System.out.println("Employee is Present");
+		}
+		else
+		{
+			System.out.println("Employee is Absent");
+		}
 	}
 
 }
